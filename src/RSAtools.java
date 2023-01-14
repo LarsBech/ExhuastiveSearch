b512dd4973ba78c3ac9bde8719abc8108330d632
LarsBech <48246361+LarsBech@users.noreply.github.com>
2023-01-14 17:46:37 +0100

    Initial commit

---
+++ src/RSAtools.java
@@ -0,0 +1,31 @@
+public class RSAtools {
+    private class KeyPair {
+        int k;
+        int n;
+
+        public KeyPair(int salt) {
+            this.k = 7;
+            this.n = 42;
+        }
+
+        public int getK() {
+            return k;
+        }
+
+        public int getN() {
+            return n;
+        }
+    }
+
+    private static int convertTextToInt(String m) {
+        int mAsEncoded = 0;
+        return mAsEncoded;
+    }
+
+    // public static
+
+    public static int encode(int k, String m) {
+        int c = 0;
+        return c;
+    }
+}
