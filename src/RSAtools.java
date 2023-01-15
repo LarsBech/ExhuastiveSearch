6babf6498932d54f54bff4c92422ef2ce1d4656b
LarsBech <48246361+LarsBech@users.noreply.github.com>
2023-01-15 14:47:48 +0100

    Working on text to int and int to text

--- src/RSAtools.java
+++ src/RSAtools.java
@@ -23,13 +23,22 @@ public class RSAtools {
         }
     }
 
-    private static int convertTextToInt(String m) {
+    public static int convertTextToInt(String m) {
         int mAsEncoded = 0;
+        for(int i=0; i<m.length(); i++){
+            char c = m.charAt(i);
+         //   mAsEncoded = c;//mAsEncoded<<(8*i) + c;
+            mAsEncoded = (mAsEncoded<<(8))+c;
+            System.out.println(c + " mAsEncoded: " + mAsEncoded);
+        }
+
         return mAsEncoded;
     }
 
-    private static String convertIntToText(int c) {
-        return "";
+    public static String convertIntToText(int c) {
+        String m ="";
+        
+        return m;
     }
 
     public static int encode(KeyPair kp, String m) {
